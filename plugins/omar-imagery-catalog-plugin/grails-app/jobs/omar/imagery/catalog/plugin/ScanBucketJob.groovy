523bc8b33d6069d13cc80636e30e783c22a2effc
sbortman <scott.bortman@gmail.com>
2018-10-05 14:20:07 -0400

    Refactored,  moving as much non-framework stuff to the library project

--- plugins/omar-imagery-catalog-plugin/grails-app/jobs/omar/imagery/catalog/plugin/ScanBucketJob.groovy
+++ plugins/omar-imagery-catalog-plugin/grails-app/jobs/omar/imagery/catalog/plugin/ScanBucketJob.groovy
@@ -1,19 +1,9 @@
 package omar.imagery.catalog.plugin
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.GetObjectRequest
-import com.amazonaws.services.s3.model.GetObjectMetadataRequest
-import com.amazonaws.services.s3.model.ListObjectsV2Request
-import com.amazonaws.services.s3.model.ListObjectsV2Result
-
-import static groovyx.gpars.GParsPool.withPool
+import omar.imagery.catalog.lib.aws.BucketScanner
 
 class ScanBucketJob
 {
-	public static final SCAN_INCREMENTAL = "incremental"
-	public static final SCAN_DEEP = "deep"
 	
 	static triggers = {}
 	
@@ -25,69 +15,6 @@ class ScanBucketJob
 		String profileName = context.mergedJobDataMap.profileName
 		String bucketName = context.mergedJobDataMap.bucketName
 		
-		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-			.withCredentials( new ProfileCredentialsProvider( profileName ) )
-//			.withRegion( clientRegion )
-			.build()
-		
-		ListObjectsV2Request request = new ListObjectsV2Request()
-			.withBucketName( bucketName )
-			.withDelimiter( '/' )
-		
-		if ( scanType == SCAN_INCREMENTAL )
-		{
-			String startAfter = 'I00000140895_01'
-			
-			request = request.withStartAfter( startAfter )
-		}
-		
-		ListObjectsV2Result result
-		
-		def start = System.currentTimeMillis()
-		
-		def mountPoint = "/data/${ bucketName }"
-		def logFile = new File( '/tmp/prefixes.txt' )
-		
-		logFile.withWriter { out ->
-			withPool {
-				while ( ( result = s3Client.listObjectsV2( request ) )?.isTruncated() )
-				{
-					result?.commonPrefixes?.eachParallel { prefix ->
-						def key = "${ prefix }${ prefix[0..<-1] }_README.XML"
-						def fullObject
-						try
-						{
-							def getObjectMetadataRequest = new GetObjectMetadataRequest( bucketName, key )
-							def objectMetadata = s3Client.getObjectMetadata( getObjectMetadataRequest )
-							
-							if ( !objectMetadata?.storageClass )
-							{
-								fullObject = s3Client.getObject( new GetObjectRequest( bucketName, key ) )
-								
-								def readMe = new XmlSlurper().parse( fullObject?.objectContent )
-								
-								out.println "${ prefix },${ readMe.NWLONG.text() },${ readMe.SELAT.text() },${ readMe.SELONG.text() },${ readMe.NWLAT.text() },${ readMe.COLLECTIONSTART.text() },${ readMe.COLLECTIONSTOP.text() }"
-							}
-							
-						}
-						catch ( e )
-						{
-							System.err.println "${ key } ${ e.message }"
-						}
-						finally
-						{
-							fullObject?.close()
-						}
-					}
-					
-					request?.continuationToken = result.nextContinuationToken
-				}
-			}
-			out.flush()
-		}
-		
-		def stop = System.currentTimeMillis()
-		
-		println "${ stop - start }"
+		BucketScanner.scan( scanType, bucketName, profileName )
 	}
 }
