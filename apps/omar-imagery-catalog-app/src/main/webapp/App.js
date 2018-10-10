00e3758ada54cd1b30e2fb03ac539626fa73b260
melbsurfer <melbsurfer@gmail.com>
2018-10-09 21:25:13 -0400

    Added notifications for scans, and error handling.

--- apps/omar-imagery-catalog-app/src/main/webapp/App.js
+++ apps/omar-imagery-catalog-app/src/main/webapp/App.js
@@ -22,43 +22,30 @@ class App extends Component {
   //     }
   //   };
   // }
-  notify = (type, time) =>
-    toast.success(`${type} scan started successfully at ${time} `, {
+  notify = (type, date) => {
+    const notifyDate = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    toast.success(`${type} scan started successfully on ${notifyDate} `, {
       position: toast.POSITION.BOTTOM_CENTER
     });
-
-  handleDeepScan = () => {
-    console.log("Handling Deep Scan!");
-
-    fetch("http://localhost:8080/bucket/scanBucket", {
-      method: "post",
-      body: JSON.stringify({ scanType: `deep` })
-    })
-      .then(r => r.json())
-      .then(json => {
-        if (json.error === 500) {
-          console.log("Server error!");
-        }
-
-        this.notify("Deep", json.timeStarted);
-      })
-      .catch(error => console.error("Error connecting to server: " + error));
   };
 
-  handleIncrementalScan = () => {
-    console.log("Handling Incremental Scan!");
+  handleScan = type => {
+    console.log(`Handling ${type} Scan!`);
 
     fetch("http://localhost:8080/bucket/scanBucket", {
       method: "post",
-      body: JSON.stringify({ scanType: `incremental` })
+      body: JSON.stringify({ scanType: type })
     })
-      .then(r => r.json())
-      .then(json => {
-        if (json.error === 500) {
-          console.log("Server error!");
+      .then(r => {
+        if (r.status === 500) {
+          toast.error(r.status + " error while connecting to server", {
+            position: toast.POSITION.BOTTOM_CENTER
+          });
         }
-
-        this.notify("Incremental", json.timeStarted);
+        r.json();
+      })
+      .then(json => {
+        this.notify(type, json.timeStarted);
       })
       .catch(error => console.error("Error connecting to server: " + error));
   };
@@ -74,10 +61,13 @@ class App extends Component {
               <div className="col">
                 <h1>OMAR Imagery Catalog</h1>
               </div>
-              <Button color="primary" onClick={this.handleDeepScan}>
+              <Button color="primary" onClick={() => this.handleScan("Deep")}>
                 Deep
               </Button>
-              <Button color="primary" onClick={this.handleIncrementalScan}>
+              <Button
+                color="primary"
+                onClick={() => this.handleScan("Incremental")}
+              >
                 Incremental
               </Button>
             </div>
