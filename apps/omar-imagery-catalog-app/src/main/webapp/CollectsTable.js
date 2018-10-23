6202a885bdede4293cfc314a33c2480a992ab0be
melbsurfer <melbsurfer@gmail.com>
2018-10-23 11:02:13 -0400

    Added react table from npm

--- apps/omar-imagery-catalog-app/src/main/webapp/CollectsTable.js
+++ apps/omar-imagery-catalog-app/src/main/webapp/CollectsTable.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+// Import React Table
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 const CollectsTable = () => {
-
   return (
     <div>
       <p>React-Table goes here!</p>
     </div>
   );
-}
+};
 
 export default CollectsTable;
