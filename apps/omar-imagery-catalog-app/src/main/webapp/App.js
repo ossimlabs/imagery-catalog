479028fe9c13075dcbfeeaea6779c5c380a8f91b
melbsurfer <melbsurfer@gmail.com>
2018-10-25 13:18:14 -0400

    Adding error boundary

--- apps/omar-imagery-catalog-app/src/main/webapp/App.js
+++ apps/omar-imagery-catalog-app/src/main/webapp/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 import MapCatalog from "./MapCatalog";
 import TestRoute from "./TestRoute";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={`/mapCatalog`} component={MapCatalog} />
-        <Route exact path={`/testRoute/:id`} component={TestRoute} />
-      </Switch>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Switch>
+          <Route exact path={`/mapCatalog`} component={MapCatalog} />
+          <Route exact path={`/testRoute/:id`} component={TestRoute} />
+        </Switch>
+      </Router>
+    </ErrorBoundary>
   );
 };
 
